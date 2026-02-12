e864b3a705dbd29a2a65b9f5f5ee3738c6f3301b
DavFilsDev <miharisoadavidfils@gmail.com>
2026-02-12 21:48:18 +0300

    feat(network-monitor level 5): add main program for network monitor

--- level-5-advanced-toolbox/network-monitor/src/main.c
+++ level-5-advanced-toolbox/network-monitor/src/main.c
@@ -2,14 +2,20 @@
 #include "connection_list.h"
 #include "stats.h"
 #include "alert.h"
+#include "config.h"
 
 int main() {
     print_banner();
+
+    Config cfg = load_config();
     ConnectionList list = get_connections();
     show_connections(list);
+
     NetworkStats stats = compute_stats(list);
     show_stats(stats);
+
     check_alerts(list, stats);
+
     free_connections(list);
     return 0;
 }
